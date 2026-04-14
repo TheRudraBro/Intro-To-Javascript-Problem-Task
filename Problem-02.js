03e378742b12baf97bb20e9318e094f3d72c15b7
TheRudraBro <rudramraj22@gmail.com>
2026-04-14 10:56:45 +0600

    Task 2 solved

--- Problem-02.js
+++ Problem-02.js
@@ -8,3 +8,7 @@ var totalMarks = mathmatics + biology + chemistry + physics + bangla;
 var averageMarks = totalMarks / 5;
 console.log("Total Marks: " + totalMarks);
 console.log("Average Marks: " + averageMarks.toFixed(2));
+
+// The total marks are calculated by summing up the marks of all subjects, 
+// and the average is calculated by dividing the total marks by the number of subjects (5). 
+// The average is then rounded to 2 decimal places using the toFixed() method.
