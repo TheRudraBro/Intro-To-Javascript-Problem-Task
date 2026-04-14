076cc2664222cc471fd63e14f73df56ea59ebe05
TheRudraBro <rudramraj22@gmail.com>
2026-04-14 10:50:48 +0600

    Updated

---
+++ Problem-02.js
@@ -0,0 +1,10 @@
+var mathmatics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = mathmatics + biology + chemistry + physics + bangla;
+var averageMarks = totalMarks / 5;
+console.log("Total Marks: " + totalMarks);
+console.log("Average Marks: " + averageMarks.toFixed(2));
